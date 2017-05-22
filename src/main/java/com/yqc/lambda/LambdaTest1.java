cf764f91ee5f53860ee1629bb8c00049676f7c0e
yangqc <yangqc_ars@outlook.com>
2017-05-22 23:23:51 +0800

    lambda表达式错误

--- src/main/java/com/yqc/lambda/LambdaTest1.java
+++ src/main/java/com/yqc/lambda/LambdaTest1.java
@@ -2,14 +2,27 @@ package com.yqc.lambda;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by yangqc on 2017/5/22.
  */
 public class LambdaTest1 {
 
-    public static void main(String[] args) {
+    public List<Dog> animalToDog() {
         List list = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            list.add(new Animal("dog" + i, i));
+        }
+
+        List dogList = list.stream().map(s -> {
+            Animal animal = (Animal) s;
+            return new Dog(animal.getName(), animal.getAge());
+        }).collect(Collectors.toList());
+        return dogList;
+    }
+
+    public static void main(String[] args) {
 
     }
 }
