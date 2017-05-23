bcde325bbf334878f6d27ee5a01d489e6f584d19
yangqc <yangqc_ars@outlook.com>
2017-05-23 23:56:04 +0800

    带参数函数式接口

--- src/main/java/com/yqc/lambda/ParameterInterface.java
+++ src/main/java/com/yqc/lambda/ParameterInterface.java
@@ -4,8 +4,7 @@ package com.yqc.lambda;
  * Created by yangqc on 2017/5/23.
  */
 @FunctionalInterface
-public interface CalculateInterface {
+public interface ParameterInterface {
 
-    void calculate();
-
+    int showParameter(int x, int y);
 }
