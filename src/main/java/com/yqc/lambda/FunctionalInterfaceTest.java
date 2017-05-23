be681a28581a3a22645647f040a2613b9f995ec4
yangqc <yangqc_ars@outlook.com>
2017-05-23 23:59:33 +0800

    带参数函数式接口

--- src/main/java/com/yqc/lambda/FunctionalInterfaceTest.java
+++ src/main/java/com/yqc/lambda/FunctionalInterfaceTest.java
@@ -5,13 +5,22 @@ package com.yqc.lambda;
  */
 public class FunctionalInterfaceTest {
 
+    public static final int x = 1;
+    public static final int y = 20;
+
     public static void showCalculateResult(CalculateInterface calculateInterface) {
         calculateInterface.calculate();
+    }
+
+    public static void showParameterResult(ParameterInterface parameterInterface) {
+        System.out.println(parameterInterface.showParameter(1, 20));
     }
 
     public static void main(String[] args) {
         showCalculateResult(() -> {
             System.out.println("ÆÚ³É!");
         });
+
+        showParameterResult((x, y) -> x + y);
     }
 }
